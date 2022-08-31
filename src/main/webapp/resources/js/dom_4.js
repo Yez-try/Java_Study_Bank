0db4bbc50fc4246a9d02bf72577dd4d6d627b9cc
simyeji <skeosk424@gmail.com>
2022-08-31 09:37:11 +0900

    0831 체크박스 해설

--- src/main/webapp/resources/js/dom_4.js
+++ src/main/webapp/resources/js/dom_4.js
@@ -8,19 +8,47 @@ for(let i=0;i<ch.length;i++){
         alert(ch[i].value);
     })
 }
+//전체선택 체크 및 해제
+// all.onclick = function(){
+//     if(all.checked==true){
+//         for(let i = 0; i<cb.length; i++){
+//             cb[i].checked = true;
+//         }
+//     }
+//     if(all.checked==false){
+//         for(let i = 0; i<cb.length; i++){
+//             cb[i].checked = false;
+//         }
+//     }
+// }
 
+// 일부 선택 해제시 전체선택 체크 해제
+// for(let i=0;i<cb.length;i++){
+//     cb[i].addEventListener("click", function(){
+//         if(cb[i].checked==false){
+//             all.checked=false;
+//         }
+//     })
+// }
+//강사님 코드 전체선택 체크 및 해제
 all.onclick = function(){
-    if(all.checked==true){
-        for(let i = 0; i<cb.length; i++){
-            cb[i].checked = true;
-        }
+    for(let i = 0; i<cb.length; i++){
+        cb[i].checked = all.checked;
     }
 }
 
-for(let i=0;i<cb.length;i++){
-    cb[i].addEventListener("click", function(){
-        if(cb[i].checked==false){
-            all.checked=false;
+//강사님 코드 
+for(let i = 0; i<cb.length;i++){
+    cb[i].addEventListener("click",check);
+}
+// 강사님 코드 일부 선택 해제시 전체선택 체크 해제
+function check(){
+    let result = true;
+    for(let i=0;i<cb.length;i++){
+        if(!cb[i].checked){
+            result=false;
+            break;
         }
-    })
+    }
+    all.checked=result
 }
